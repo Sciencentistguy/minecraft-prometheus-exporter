654047c4b367b6f5524b35ccac641523b1a4306b
Jamie Quigley <jamie@quigley.xyz>
2021-09-12 22:47:26 +0100

    Removed a bunch of `unwrap()`

--- src/main.rs
+++ src/main.rs
@@ -29,16 +29,16 @@ struct Stats {
     killed_by: Option<HashMap<String, usize>>,
 }
 
-async fn read_json(path: impl AsRef<Path>) -> Stats {
+async fn read_json(path: impl AsRef<Path>) -> Result<Stats> {
     #[derive(Debug, Deserialize)]
     struct Stats_ {
         stats: Stats,
         #[serde(rename = "DataVersion")]
         data_version: u64,
     }
-    let file = tokio::fs::read_to_string(path.as_ref()).await.unwrap();
-    let json: Stats_ = serde_json::from_str(file.as_str()).unwrap();
-    json.stats
+    let file = tokio::fs::read_to_string(path.as_ref()).await?;
+    let json: Stats_ = serde_json::from_str(file.as_str())?;
+    Ok(json.stats)
 }
 
 #[derive(Clone)]
@@ -71,200 +71,175 @@ async fn get_player_name(uuid: &str) -> Result<String> {
     Ok(recent_name.name)
 }
 
-async fn scrape_server<P: AsRef<Path>>(
-    stats_path: P,
-    server_name: &str,
-) -> core::result::Result<String, warp::Rejection> {
+async fn scrape_server<P: AsRef<Path>>(stats_path: P, server_name: &str) -> Result<String> {
     let mut output = String::new();
-    for (server_file_path, server_name) in vec![(
-        "/dev/shm/stats".to_owned(),
-        "the_server_to_end_all_servers".to_owned(),
-    )]
-    .iter()
-    .map(|(x, y)| (x.as_str(), y.as_str()))
-    {
-        info!(%server_name, "Starting scrape");
-        for file in std::fs::read_dir(server_file_path).unwrap() {
-            let file = file.unwrap();
-            if !file.file_type().unwrap().is_file() {
-                continue;
-            }
-            let file_path = file.path();
-            let player_uuid = file_path.file_stem().unwrap().to_str().unwrap();
-            let player_name = get_player_name(player_uuid).await.unwrap();
-            debug!(uuid = %player_uuid, name = %player_name, "Scraping player");
-            let json = read_json(&file.path()).await;
-            if let Some(ref dropped) = json.dropped {
-                trace!("Scraping minecraft:dropped");
-                writeln!(
-                    output,
-                    "# HELP minecraft_items_dropped minecraft-prometheus-exporter"
-                )
-                .unwrap();
-                writeln!(output, "# TYPE minecraft_items_dropped counter").unwrap();
+    info!(%server_name, "Starting scrape");
+    for file in std::fs::read_dir(stats_path)? {
+        let file = file?;
+        if !file.file_type()?.is_file() {
+            continue;
+        }
+        let file_path = file.path();
+        let player_uuid = file_path
+            .file_stem()
+            .ok_or_else(|| eyre::eyre!("no file stem"))?
+            .to_str()
+            .expect("filename is a uuid so should be valid unicode.");
+        let player_name = get_player_name(player_uuid).await?;
+        debug!(uuid = %player_uuid, name = %player_name, "Scraping player");
+        let json = read_json(&file.path()).await?;
+        if let Some(ref dropped) = json.dropped {
+            trace!("Scraping minecraft:dropped");
+            writeln!(
+                output,
+                "# HELP minecraft_items_dropped minecraft-prometheus-exporter"
+            )?;
+            writeln!(output, "# TYPE minecraft_items_dropped counter")?;
 
-                for (name, &value) in dropped {
-                    writeln!(
-                        output,
-                        "minecraft_items_dropped{{\
-                                 server=\"{}\", player=\"{}\", item=\"{}\"\
-                                 }} {}",
-                        server_name, player_name, name, value
-                    )
-                    .unwrap()
-                }
-            }
-            if let Some(ref crafted) = json.crafted {
-                trace!("Scraping minecraft:crafted");
+            for (name, &value) in dropped {
                 writeln!(
                     output,
-                    "# HELP minecraft_items_crafted minecraft-prometheus-exporter"
-                )
-                .unwrap();
-                writeln!(output, "# TYPE minecraft_items_crafted counter").unwrap();
-                for (name, &value) in crafted {
-                    writeln!(
-                        output,
-                        "minecraft_items_crafted{{\
-                                 server=\"{}\", player=\"{}\", item=\"{}\"\
-                                 }} {}",
-                        server_name, player_name, name, value
-                    )
-                    .unwrap()
-                }
+                    "minecraft_items_dropped{{\
+                         server=\"{}\", player=\"{}\", item=\"{}\"\
+                         }} {}",
+                    server_name, player_name, name, value
+                )?;
             }
-            if let Some(ref killed) = json.killed {
-                trace!("Scraping minecraft:killed");
+        }
+        if let Some(ref crafted) = json.crafted {
+            trace!("Scraping minecraft:crafted");
+            writeln!(
+                output,
+                "# HELP minecraft_items_crafted minecraft-prometheus-exporter"
+            )?;
+            writeln!(output, "# TYPE minecraft_items_crafted counter")?;
+            for (name, &value) in crafted {
                 writeln!(
                     output,
-                    "# HELP minecraft_entities_killed minecraft-prometheus-exporter"
-                )
-                .unwrap();
-                writeln!(output, "# TYPE minecraft_entities_killed counter").unwrap();
-                for (name, &value) in killed {
-                    writeln!(
-                        output,
-                        "minecraft_entities_killed{{\
-                                 server=\"{}\", player=\"{}\", entity=\"{}\"\
-                                 }} {}",
-                        server_name, player_name, name, value
-                    )
-                    .unwrap()
-                }
+                    "minecraft_items_crafted{{\
+                         server=\"{}\", player=\"{}\", item=\"{}\"\
+                         }} {}",
+                    server_name, player_name, name, value
+                )?;
             }
-            if let Some(ref broken) = json.broken {
-                trace!("Scraping minecraft:broken");
+        }
+        if let Some(ref killed) = json.killed {
+            trace!("Scraping minecraft:killed");
+            writeln!(
+                output,
+                "# HELP minecraft_entities_killed minecraft-prometheus-exporter"
+            )?;
+            writeln!(output, "# TYPE minecraft_entities_killed counter")?;
+            for (name, &value) in killed {
                 writeln!(
                     output,
-                    "# HELP minecraft_blocks_broken minecraft-prometheus-exporter"
-                )
-                .unwrap();
-                writeln!(output, "# TYPE minecraft_blocks_broken counter").unwrap();
-                for (name, &value) in broken {
-                    writeln!(
-                        output,
-                        "minecraft_blocks_broken{{\
-                                 server=\"{}\", player=\"{}\", block=\"{}\"\
-                                 }} {}",
-                        server_name, player_name, name, value
-                    )
-                    .unwrap()
-                }
+                    "minecraft_entities_killed{{\
+                         server=\"{}\", player=\"{}\", entity=\"{}\"\
+                         }} {}",
+                    server_name, player_name, name, value
+                )?;
             }
-            if let Some(ref used) = json.used {
-                trace!("Scraping minecraft:used");
+        }
+        if let Some(ref broken) = json.broken {
+            trace!("Scraping minecraft:broken");
+            writeln!(
+                output,
+                "# HELP minecraft_blocks_broken minecraft-prometheus-exporter"
+            )?;
+            writeln!(output, "# TYPE minecraft_blocks_broken counter")?;
+            for (name, &value) in broken {
                 writeln!(
                     output,
-                    "# HELP minecraft_items_used minecraft-prometheus-exporter"
-                )
-                .unwrap();
-                writeln!(output, "# TYPE minecraft_items_used counter").unwrap();
-                for (name, &value) in used {
-                    writeln!(
-                        output,
-                        "minecraft_items_used{{\
-                                 server=\"{}\", player=\"{}\", item=\"{}\"\
-                                 }} {}",
-                        server_name, player_name, name, value
-                    )
-                    .unwrap()
-                }
+                    "minecraft_blocks_broken{{\
+                         server=\"{}\", player=\"{}\", block=\"{}\"\
+                         }} {}",
+                    server_name, player_name, name, value
+                )?;
             }
-            if let Some(ref mined) = json.mined {
-                trace!("Scraping minecraft:mined");
+        }
+        if let Some(ref used) = json.used {
+            trace!("Scraping minecraft:used");
+            writeln!(
+                output,
+                "# HELP minecraft_items_used minecraft-prometheus-exporter"
+            )?;
+            writeln!(output, "# TYPE minecraft_items_used counter")?;
+            for (name, &value) in used {
                 writeln!(
                     output,
-                    "# HELP minecraft_blocks_mined minecraft-prometheus-exporter"
-                )
-                .unwrap();
-                writeln!(output, "# TYPE minecraft_blocks_mined counter").unwrap();
-                for (name, &value) in mined {
-                    writeln!(
-                        output,
-                        "minecraft_blocks_mined{{\
-                                 server=\"{}\", player=\"{}\", block=\"{}\"\
-                                 }} {}",
-                        server_name, player_name, name, value
-                    )
-                    .unwrap()
-                }
+                    "minecraft_items_used{{\
+                         server=\"{}\", player=\"{}\", item=\"{}\"\
+                         }} {}",
+                    server_name, player_name, name, value
+                )?;
             }
-            if let Some(ref custom) = json.custom {
-                trace!("Scraping minecraft:custom");
+        }
+        if let Some(ref mined) = json.mined {
+            trace!("Scraping minecraft:mined");
+            writeln!(
+                output,
+                "# HELP minecraft_blocks_mined minecraft-prometheus-exporter"
+            )?;
+            writeln!(output, "# TYPE minecraft_blocks_mined counter")?;
+            for (name, &value) in mined {
                 writeln!(
                     output,
-                    "# HELP minecraft_custom minecraft-prometheus-exporter"
-                )
-                .unwrap();
-                writeln!(output, "# TYPE minecraft_custom counter").unwrap();
-                for (name, &value) in custom {
-                    writeln!(
-                        output,
-                        "minecraft_custom{{\
-                                 server=\"{}\", player=\"{}\", item=\"{}\"\
-                                 }} {}",
-                        server_name, player_name, name, value
-                    )
-                    .unwrap()
-                }
+                    "minecraft_blocks_mined{{\
+                         server=\"{}\", player=\"{}\", block=\"{}\"\
+                         }} {}",
+                    server_name, player_name, name, value
+                )?;
             }
-            if let Some(ref picked_up) = json.picked_up {
-                trace!("Scraping minecraft:picked_up");
+        }
+        if let Some(ref custom) = json.custom {
+            trace!("Scraping minecraft:custom");
+            writeln!(
+                output,
+                "# HELP minecraft_custom minecraft-prometheus-exporter"
+            )?;
+            writeln!(output, "# TYPE minecraft_custom counter")?;
+            for (name, &value) in custom {
                 writeln!(
                     output,
-                    "# HELP minecraft_items_picked_up minecraft-prometheus-exporter"
-                )
-                .unwrap();
-                writeln!(output, "# TYPE minecraft_items_picked_up counter").unwrap();
-                for (name, &value) in picked_up {
-                    writeln!(
-                        output,
-                        "minecraft_items_picked_up{{\
-                                 server=\"{}\", player=\"{}\", item=\"{}\"\
-                                 }} {}",
-                        server_name, player_name, name, value
-                    )
-                    .unwrap()
-                }
+                    "minecraft_custom{{\
+                         server=\"{}\", player=\"{}\", item=\"{}\"\
+                         }} {}",
+                    server_name, player_name, name, value
+                )?;
             }
-            if let Some(ref killed_by) = json.killed_by {
-                trace!("Scraping minecraft:killed_by");
+        }
+        if let Some(ref picked_up) = json.picked_up {
+            trace!("Scraping minecraft:picked_up");
+            writeln!(
+                output,
+                "# HELP minecraft_items_picked_up minecraft-prometheus-exporter"
+            )?;
+            writeln!(output, "# TYPE minecraft_items_picked_up counter")?;
+            for (name, &value) in picked_up {
                 writeln!(
                     output,
-                    "# HELP minecraft_entities_killed_by minecraft-prometheus-exporter"
-                )
-                .unwrap();
-                writeln!(output, "# TYPE minecraft_entities_killed_by counter").unwrap();
-                for (name, &value) in killed_by {
-                    writeln!(
-                        output,
-                        "minecraft_entities_killed_by{{\
-                                 server=\"{}\", player=\"{}\", entity=\"{}\"\
-                                 }} {}",
-                        server_name, player_name, name, value
-                    )
-                    .unwrap()
-                }
+                    "minecraft_items_picked_up{{\
+                         server=\"{}\", player=\"{}\", item=\"{}\"\
+                         }} {}",
+                    server_name, player_name, name, value
+                )?;
+            }
+        }
+        if let Some(ref killed_by) = json.killed_by {
+            trace!("Scraping minecraft:killed_by");
+            writeln!(
+                output,
+                "# HELP minecraft_entities_killed_by minecraft-prometheus-exporter"
+            )?;
+            writeln!(output, "# TYPE minecraft_entities_killed_by counter")?;
+            for (name, &value) in killed_by {
+                writeln!(
+                    output,
+                    "minecraft_entities_killed_by{{\
+                         server=\"{}\", player=\"{}\", entity=\"{}\"\
+                         }} {}",
+                    server_name, player_name, name, value
+                )?;
             }
         }
     }
@@ -272,14 +247,13 @@ async fn scrape_server<P: AsRef<Path>>(
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     tracing::subscriber::set_global_default(
         tracing_subscriber::FmtSubscriber::builder()
             .with_max_level(Level::TRACE)
             .pretty()
             .finish(),
-    )
-    .unwrap();
+    )?;
 
     let metric = warp::path("metric").and_then(|| async move {
         if false {
@@ -294,7 +268,15 @@ async fn main() {
         .iter()
         .map(|(x, y)| (x.as_str(), y.as_str()))
         {
-            output.push_str(scrape_server(server_file_path, server_name).await?.as_str());
+            output.push_str(
+                scrape_server(server_file_path, server_name)
+                    .await
+                    .map_err(|e| {
+                        error!(error = ?e, "");
+                        warp::reject::reject()
+                    })?
+                    .as_str(),
+            );
         }
 
         Ok(output)
@@ -302,4 +284,5 @@ async fn main() {
 
     info!("Started listening on "); // TODO config
     warp::serve(metric).run(([127, 0, 0, 1], 3030)).await;
+    Ok(())
 }
